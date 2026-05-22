3b387966b6af2171e2eba864cf5a30e65e2f7658
Isaac Olorunfemi <isaacolorunfemi.dev@gmail.com>
2026-05-22 10:11:09 +0100

    Comment out cloudinary installation in build.sh
    Comment out cloudinary installation commands in build script.

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,5 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-pip install cloudinary 
-pip install django-cloudinary-storage 
+#pip install cloudinary 
+#pip install django-cloudinary-storage 
