437af6c7dc143b53c8e43006f33fef294875bc63
Isaac Olorunfemi <isaacolorunfemi.dev@gmail.com>
2026-05-22 09:11:17 +0100

    Add Cloudinary and Pillow dependencies to build script

--- backend/build.sh
+++ backend/build.sh
@@ -2,3 +2,6 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+pip install cloudinary 
+pip install django-cloudinary-storage 
+pip install Pillow
