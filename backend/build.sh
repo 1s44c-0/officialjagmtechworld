9b953065e1e566eaa0b66ecf193d391ce7db6b7a
Isaac Olorunfemi <isaacolorunfemi.dev@gmail.com>
2026-05-22 10:47:28 +0100

    Remove --noinput from collectstatic command

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python manage.py collectstatic
 python manage.py migrate
 pip install cloudinary 
 pip install django-cloudinary-storage 
